62cc35eae273af37e6881f61eff0ef9f615e5eeb
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2023-10-25 14:27:09 -0300

    chore: initial commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return { hello: tables }
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
