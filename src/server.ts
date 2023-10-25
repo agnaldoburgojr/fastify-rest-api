df573a937767225fcc12aa7010ef4dc349b9579c
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2023-10-25 15:23:27 -0300

    chore: add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,25 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return { hello: tables }
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 100,
+    })
+    .returning('*')
+
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333')
